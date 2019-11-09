adced977d109e2164ab89977669f5d616ec4709c
Kneeslapper <gloriaklee8@gmail.com>
2019-11-09 01:40:12 -0800

    added requirements, attempted logic, not able to complete subtracting

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,5 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-
-
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,16 +7,65 @@ var connection = mysql.createConnection({
     password: "test",
     database: "bamazon_db"
 });
-
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     afterConnection();
 });
-
 function afterConnection() {
     connection.query("SELECT * FROM products", function (err, respond) {
         if (err) throw err;
-        console.log(respond);
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Place the ID of the product you would like to buy: ",
+                name: "product"
+            },
+            {
+                type: "input",
+                message: "How many units would you like to buy?",
+                name: "quantity"
+            }
+        ])
+            .then(function (resUnit) {
+                console.log(resUnit.quantity);
+                var pass = resUnit;
+                fillOrder(pass)
+
+            })
     });
+}
+function fillOrder(item) {
+    var thing = item
+    connection.query(
+        "SELECT * FROM products WHERE item_id=" + parseInt(item.product), function (err, results) {
+            console.log(typeof results[0].stock_quantity)
+            if (err) throw (err);
+            if (results[0].stock_quantity > parseInt(item.quantity)) {
+                console.log("instock!")
+                updateOrder(thing);
+            }
+        }
+    )
+}
+function updateOrder(product) {
+    console.log(product)
+    var num = parseInt(product.quantity);
+    console.log(num)
+    connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: -parseInt(num)
+            },
+            {
+                item_id: parseInt(product.product)
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+            console.log(res)
+            // console.log(res.affectedRows + "products updated! \n");
+        }
+    )
 }
