439e17fb7de65ee6f78c135cf912d1b62bbe28e6
Kneeslapper <gloriaklee8@gmail.com>
2019-11-25 17:33:36 -0800

    added sql

---
+++ bamazon.sql
@@ -0,0 +1,23 @@
+USE bamazon_db;
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("fork", "kitchen","1","90");
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("spoon", "kitchen", "1", "85");
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("water", "food", "2", "100");
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("gum", "food", "5", "50");
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("mirror", "cosmedic", "3", "25");
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("comb", "cosmedic", "2", "40");
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("tweezer", "cosmedic", "1", "25");
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("broom", "house hold", "6", "28");
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("tv", "entertainment", "70", "100");
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("radio", "entertainment", "20", "4");
+
+SELECT * FROM productsv
